e333da9189ad64a1f4fa2e55918cad6e9f4aeb33
Andreas Roessler <andreas.roessler@hs-esslingen.de>
2026-03-11 08:57:22 +0100

    fix

--- 10_Python/first.py
+++ 10_Python/first.py
@@ -1,2 +1,11 @@
 print("Hello World")
 print("Hello Esslingen")
+
+
+dict_test = {"key1": "value1", "key2": "value2", "key3": 15}
+
+if "key1" in dict_test:
+    print("key1 is in the dictionary")  
+
+if "key3" in dict_test:
+    print("key3 is in the dictionary")  
